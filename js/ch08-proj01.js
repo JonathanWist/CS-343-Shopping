97131baee1cb9d6fec7e293f8aac36eeef40bbb3
Jonathan Wist <jpwist@live.com>
2022-10-10 13:40:00 -0400

    changes to shopping lab

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -4,3 +4,9 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+   function outputBody(){
+for (let item of cart) {
+         let total = calculateTotal(item.quantity, item.product.price);
+         outputCartRow(item, total);
+   }
+}
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,21 @@
 /* define your functions here */
 
+function calculateTotal(quantity, price)
+{
+    return quantity * price;
+}
 
-
-
-
-
-
+function outputCartRow(item, total) 
+{
+    const {product: {title, filename, price}, quantity,} = item;
+    const row = `       <tr>
+                <td><img src="images/${filename}"></td>
+                <td>${title}</td>
+                <td>${quantity}</td>
+                <td>${price.toFixed(2)}</td>
+                <td>${total.toFixed(2)}</td>
+                </tr>`
+    document.write(row);
+}
 
         
